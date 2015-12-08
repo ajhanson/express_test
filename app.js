f6406ffeb088715b46e9a1766fd6ef77bd15843b
Aaron Hanson <aaron.hanson@yahoo.com>
2015-12-07 21:22:14 -0600

    updating static and jade

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.set('view engine', 'jade');
 //app.use(express.static('src/views'));
 
 app.get('/', function (req, res) {
-    res.render('index');
+    res.sendFile('index.html', {
+        root: __dirname + '/src/views'
+    });
 });
 
-//app.get('/', function (req, res) {
-//    res.sendfile('index.html', {
-//        root: __dirname + '/src/views'
-//    });
-//});
+app.get('/jade', function (req, res) {
+    res.render('index');
+});
 
 app.listen(port, function (err) {
     console.log('running server on port ' + port);
