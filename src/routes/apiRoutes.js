83c1b79480a45bcfdf57f60b55cb0c22fd67cf9c
ajhanson <aaron.hanson@yahoo.com>
2015-12-09 23:47:50 -0600

    adding simple mongodb

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,20 +1,36 @@
 var express = require('express');
+var mongodb = require('mongodb').MongoClient;
 
 var apiRouter = express.Router();
+var url = 'mongodb://localhost:27017/posts'
 
 var router = function () {
 
     apiRouter.route('/posts')
         .get(function (req, res) {
-                res.json({
-                        posts: [{
-                            title: 'title 1',
-                            author: 'author 1'
-                        }]
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('posts');
+                collection.find({}).toArray(function (err, results) {
+                    res.send(results);
+                    db.close();
                 });
+            });
+        })
+        .post(function (req, res) {
+            console.log(req.content);
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('posts');
+                collection.insertOne({
+                    post: req.content,
+                    author: req.author
+                }, function (err, results) {
+                    res.send(true);
+                    db.close();
+                });
+            });
         });
 
-return apiRouter;
+    return apiRouter;
 
 };
 
